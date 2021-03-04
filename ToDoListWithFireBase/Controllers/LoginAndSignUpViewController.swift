5631436b28f04f444eb6fd0799fe04de7b7deea5
Dmitri Breshchuk <breschuk1@gmail.com>
2021-03-04 22:29:15 +0300

    Add UI staff for Login screen

--- ToDoListWithFireBase/Controllers/LoginAndSignUpViewController.swift
+++ ToDoListWithFireBase/Controllers/LoginAndSignUpViewController.swift
@@ -9,18 +9,58 @@ import UIKit
 
 class LoginAndSignUpViewController: UIViewController {
 
-    var textLabel = UILabel()
+    var registrationLabel = UILabel()
+    var emailTextField = UITextField()
+    var passTextField = UITextField()
+    var secondPassTextField = UITextField()
+    var signInButton = UIButton()
+    var loginButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
-        textLabel.frame = CGRect(x: 100, y: 200, width: 100, height: 23)
-        textLabel.text = "Regist\nration"
-        textLabel.numberOfLines = 5
-        textLabel.lineBreakMode = .byWordWrapping
-        view.addSubview(textLabel)
+        //MARK: - UI(Registration Label)
+        registrationLabel.frame = CGRect(x: 10, y: 50, width: 300, height: 60)
+        registrationLabel.text = "Registration"
+        registrationLabel.font = UIFont.systemFont(ofSize: 50)
+        registrationLabel.shadowOffset = .init(width: 2, height: 0)
+        registrationLabel.shadowColor = .black
         
+        //MARK: - UI(Text fields)
+        emailTextField.frame = CGRect(x: 40, y: 300, width: view.frame.width - 80, height: 40)
+        emailTextField.placeholder = "Email"
+        emailTextField.borderStyle = .roundedRect
+        
+        passTextField.frame = CGRect(x: 40, y: 380, width: view.frame.width - 80, height: 40)
+        passTextField.placeholder = "Password"
+        passTextField.isSecureTextEntry = true
+        passTextField.borderStyle = .roundedRect
+        
+        secondPassTextField.frame = CGRect(x: 40, y: 460, width: view.frame.width - 80, height: 40)
+        secondPassTextField.placeholder = "Repeat password"
+        secondPassTextField.isSecureTextEntry = true
+        secondPassTextField.borderStyle = .roundedRect
+        
+        //MARK: - UI(Buttons)
+        signInButton.frame = CGRect(x: 80, y: 540, width: view.frame.width - 160, height: 40)
+        signInButton.setTitle("Sign In", for: .normal)
+        signInButton.backgroundColor = .blue
+        signInButton.setTitleColor(.white, for: .normal)
+        signInButton.addTarget(self, action: #selector(signInButtonTapped(sender:)), for: .touchUpInside)
+        
+        
+        
+        //MARK: - Add subviews
+        view.addSubview(registrationLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(passTextField)
+        view.addSubview(secondPassTextField)
+        view.addSubview(signInButton)
+    }
+    
+    @objc private func signInButtonTapped(sender: UIButton) {
+        sender.pulsate()
     }
     
 
