2149a69497a06d36b179afc60eff34365414c821
Dmitri Breshchuk <breschuk1@gmail.com>
2021-03-04 21:05:23 +0300

    Add transition to login screen

--- ToDoListWithFireBase/Controllers/RootViewController.swift
+++ ToDoListWithFireBase/Controllers/RootViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class RootViewController: UIViewController {
+    
+    var currentView: UIViewController
+    
+    init() {
+        self.currentView = SplashScreenViewController()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        
+        view.backgroundColor = .white
+        
+        addChild(currentView)
+        currentView.view.bounds = view.bounds
+        currentView.didMove(toParent: self)
+        view.addSubview(currentView.view)
     }
 
+    
+    func showLoginAndSignUpScreen() {
+        let LoginAndSignUpScreen = LoginAndSignUpViewController()
+        animatedFadeTransition(to: LoginAndSignUpScreen)
+    }
+    
+    private func animatedFadeTransition(to new: UIViewController, complition: (()->Void)? = nil) {
+        currentView.willMove(toParent: nil)
+        addChild(new)
+        transition(from: self, to: new, duration: 0.3, options: .curveEaseOut) {
+            self.currentView.removeFromParent()
+            new.didMove(toParent: self)
+            self.currentView = new
+            complition?()
+        }
+
+    }
 
 }
 
