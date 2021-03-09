8a3822863a4312766c56c3521698442850dd7b17
Dmitri Breshchuk <breschuk1@gmail.com>
2021-03-09 12:26:03 +0300

    Add some Ui for Task screen

--- ToDoListWithFireBase/Controllers/TasksViewController.swift
+++ ToDoListWithFireBase/Controllers/TasksViewController.swift
@@ -10,15 +10,26 @@ import Firebase
 
 class TasksViewController: UIViewController {
 
+   private let tableView = UITableView()
+   private let cellIdentifier = "myCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
         title = "Tasks"
+        
+        //MARK: - Nav Controller Buttons
         let signOutButton = UIBarButtonItem(title: "Sign Out", style: .plain, target: self, action: #selector(signOutButtonPressed(target:)))
         navigationController?.navigationBar.titleTextAttributes = [.font: UIFont.systemFont(ofSize: 23)]
         navigationItem.setLeftBarButton(signOutButton, animated: true)
+        
+        
+        //MARK: - Table View
+        tableView.frame = view.frame
+        tableView.center = view.center
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
 
@@ -34,4 +45,25 @@ class TasksViewController: UIViewController {
         
     }
 
+}
+
+extension TasksViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+      return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    
 }
