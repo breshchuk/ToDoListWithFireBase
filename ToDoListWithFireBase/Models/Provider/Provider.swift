39764f4b200be5909a054543ae637f7e0c857934
Dmitri Breshchuk <breschuk1@gmail.com>
2021-04-13 18:05:31 +0300

    Add providers enum

---
+++ ToDoListWithFireBase/Models/Provider/Provider.swift
@@ -0,0 +1,16 @@
+//
+//  Provider.swift
+//  ToDoListWithFireBase
+//
+//  Created by dimam on 7.04.21.
+//
+
+import Foundation
+
+enum Provider {
+    
+    case google
+    case facebook
+    case firebase
+    case withoutProvider
+}
